eabfeaba61d4ad51653e215c75ab6dfe73c7d0bb
Jawline0700 <90067793+Jawline0700@users.noreply.github.com>
2021-11-15 13:00:20 -0500

    change2

--- DS8_LAB3/RegistraCuenta.Designer.vb
+++ DS8_LAB3/RegistraCuenta.Designer.vb
@@ -23,109 +23,141 @@ Partial Class RegistraCuenta
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.txt_cedula = New System.Windows.Forms.TextBox()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.cbox_tipoCuenta = New System.Windows.Forms.ComboBox()
-        Me.txt_saldo = New System.Windows.Forms.TextBox()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.btn_crearCuenta = New System.Windows.Forms.Button()
+        Me.l1 = New Guna.UI.WinForms.GunaLabel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btn_registrar = New Guna.UI.WinForms.GunaButton()
+        Me.txt_saldo = New Bunifu.Framework.UI.BunifuMaterialTextbox()
+        Me.txt_cedula = New Bunifu.Framework.UI.BunifuMaterialTextbox()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(307, 42)
+        Me.Label1.Location = New System.Drawing.Point(409, 52)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(99, 13)
+        Me.Label1.Size = New System.Drawing.Size(0, 17)
         Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Apertura de Cuenta"
         '
-        'txt_cedula
+        'l1
         '
-        Me.txt_cedula.Location = New System.Drawing.Point(305, 85)
-        Me.txt_cedula.Name = "txt_cedula"
-        Me.txt_cedula.Size = New System.Drawing.Size(121, 20)
-        Me.txt_cedula.TabIndex = 5
+        Me.l1.AutoSize = True
+        Me.l1.Font = New System.Drawing.Font("Segoe UI", 25.0!, System.Drawing.FontStyle.Bold)
+        Me.l1.ForeColor = System.Drawing.Color.Silver
+        Me.l1.Location = New System.Drawing.Point(288, 19)
+        Me.l1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.l1.Name = "l1"
+        Me.l1.Size = New System.Drawing.Size(414, 57)
+        Me.l1.TabIndex = 11
+        Me.l1.Text = "Apertura de Cuenta"
+        Me.l1.Visible = False
         '
-        'Label2
+        'GroupBox1
         '
-        Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(209, 88)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(40, 13)
-        Me.Label2.TabIndex = 4
-        Me.Label2.Text = "Cedula"
+        Me.GroupBox1.Controls.Add(Me.btn_registrar)
+        Me.GroupBox1.Controls.Add(Me.txt_saldo)
+        Me.GroupBox1.Controls.Add(Me.txt_cedula)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Silver
+        Me.GroupBox1.Location = New System.Drawing.Point(280, 114)
+        Me.GroupBox1.Margin = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Padding = New System.Windows.Forms.Padding(4)
+        Me.GroupBox1.Size = New System.Drawing.Size(515, 399)
+        Me.GroupBox1.TabIndex = 12
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese Datos"
         '
-        'Label3
+        'btn_registrar
         '
-        Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(209, 129)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(80, 13)
-        Me.Label3.TabIndex = 6
-        Me.Label3.Text = "Tipo de Cuenta"
-        '
-        'cbox_tipoCuenta
-        '
-        Me.cbox_tipoCuenta.FormattingEnabled = True
-        Me.cbox_tipoCuenta.Location = New System.Drawing.Point(305, 126)
-        Me.cbox_tipoCuenta.Name = "cbox_tipoCuenta"
-        Me.cbox_tipoCuenta.Size = New System.Drawing.Size(121, 21)
-        Me.cbox_tipoCuenta.TabIndex = 7
+        Me.btn_registrar.Animated = True
+        Me.btn_registrar.AnimationHoverSpeed = 0.07!
+        Me.btn_registrar.AnimationSpeed = 0.03!
+        Me.btn_registrar.BaseColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(199, Byte), Integer), CType(CType(140, Byte), Integer))
+        Me.btn_registrar.BorderColor = System.Drawing.Color.Black
+        Me.btn_registrar.DialogResult = System.Windows.Forms.DialogResult.None
+        Me.btn_registrar.FocusedColor = System.Drawing.Color.Empty
+        Me.btn_registrar.Font = New System.Drawing.Font("Segoe UI", 9.0!)
+        Me.btn_registrar.ForeColor = System.Drawing.Color.White
+        Me.btn_registrar.Image = Nothing
+        Me.btn_registrar.ImageSize = New System.Drawing.Size(20, 20)
+        Me.btn_registrar.Location = New System.Drawing.Point(96, 287)
+        Me.btn_registrar.Margin = New System.Windows.Forms.Padding(4)
+        Me.btn_registrar.Name = "btn_registrar"
+        Me.btn_registrar.OnHoverBaseColor = System.Drawing.Color.SpringGreen
+        Me.btn_registrar.OnHoverBorderColor = System.Drawing.Color.Black
+        Me.btn_registrar.OnHoverForeColor = System.Drawing.Color.White
+        Me.btn_registrar.OnHoverImage = Nothing
+        Me.btn_registrar.OnPressedColor = System.Drawing.Color.Black
+        Me.btn_registrar.Size = New System.Drawing.Size(336, 52)
+        Me.btn_registrar.TabIndex = 18
+        Me.btn_registrar.Text = "Crear Cuenta "
+        Me.btn_registrar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
         '
         'txt_saldo
         '
-        Me.txt_saldo.Location = New System.Drawing.Point(305, 165)
+        Me.txt_saldo.AutoValidate = System.Windows.Forms.AutoValidate.Disable
+        Me.txt_saldo.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txt_saldo.Font = New System.Drawing.Font("Century Gothic", 9.75!)
+        Me.txt_saldo.ForeColor = System.Drawing.Color.Silver
+        Me.txt_saldo.HintForeColor = System.Drawing.Color.Silver
+        Me.txt_saldo.HintText = "Cedula"
+        Me.txt_saldo.isPassword = False
+        Me.txt_saldo.LineFocusedColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(199, Byte), Integer), CType(CType(140, Byte), Integer))
+        Me.txt_saldo.LineIdleColor = System.Drawing.Color.Gray
+        Me.txt_saldo.LineMouseHoverColor = System.Drawing.Color.Silver
+        Me.txt_saldo.LineThickness = 3
+        Me.txt_saldo.Location = New System.Drawing.Point(143, 187)
+        Me.txt_saldo.Margin = New System.Windows.Forms.Padding(5)
         Me.txt_saldo.Name = "txt_saldo"
-        Me.txt_saldo.Size = New System.Drawing.Size(121, 20)
-        Me.txt_saldo.TabIndex = 9
+        Me.txt_saldo.Size = New System.Drawing.Size(206, 54)
+        Me.txt_saldo.TabIndex = 15
+        Me.txt_saldo.Text = "Saldo"
+        Me.txt_saldo.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
         '
-        'Label4
+        'txt_cedula
         '
-        Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(209, 168)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(34, 13)
-        Me.Label4.TabIndex = 8
-        Me.Label4.Text = "Saldo"
-        '
-        'btn_crearCuenta
-        '
-        Me.btn_crearCuenta.Location = New System.Drawing.Point(305, 221)
-        Me.btn_crearCuenta.Name = "btn_crearCuenta"
-        Me.btn_crearCuenta.Size = New System.Drawing.Size(128, 23)
-        Me.btn_crearCuenta.TabIndex = 10
-        Me.btn_crearCuenta.Text = "Crear Cuenta"
-        Me.btn_crearCuenta.UseVisualStyleBackColor = True
+        Me.txt_cedula.AutoValidate = System.Windows.Forms.AutoValidate.Disable
+        Me.txt_cedula.Cursor = System.Windows.Forms.Cursors.IBeam
+        Me.txt_cedula.Font = New System.Drawing.Font("Century Gothic", 9.75!)
+        Me.txt_cedula.ForeColor = System.Drawing.Color.Silver
+        Me.txt_cedula.HintForeColor = System.Drawing.Color.Silver
+        Me.txt_cedula.HintText = "Cedula"
+        Me.txt_cedula.isPassword = False
+        Me.txt_cedula.LineFocusedColor = System.Drawing.Color.FromArgb(CType(CType(32, Byte), Integer), CType(CType(199, Byte), Integer), CType(CType(140, Byte), Integer))
+        Me.txt_cedula.LineIdleColor = System.Drawing.Color.Gray
+        Me.txt_cedula.LineMouseHoverColor = System.Drawing.Color.Silver
+        Me.txt_cedula.LineThickness = 3
+        Me.txt_cedula.Location = New System.Drawing.Point(143, 38)
+        Me.txt_cedula.Margin = New System.Windows.Forms.Padding(5)
+        Me.txt_cedula.Name = "txt_cedula"
+        Me.txt_cedula.Size = New System.Drawing.Size(206, 54)
+        Me.txt_cedula.TabIndex = 8
+        Me.txt_cedula.TextAlign = System.Windows.Forms.HorizontalAlignment.Left
         '
         'RegistraCuenta
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.btn_crearCuenta)
-        Me.Controls.Add(Me.txt_saldo)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.cbox_tipoCuenta)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.txt_cedula)
-        Me.Controls.Add(Me.Label2)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(54, Byte), Integer), CType(CType(54, Byte), Integer), CType(CType(54, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(1040, 585)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.l1)
         Me.Controls.Add(Me.Label1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "RegistraCuenta"
         Me.Text = "RegistraCuenta"
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents Label1 As Label
-    Friend WithEvents txt_cedula As TextBox
-    Friend WithEvents Label2 As Label
-    Friend WithEvents Label3 As Label
-    Friend WithEvents cbox_tipoCuenta As ComboBox
-    Friend WithEvents txt_saldo As TextBox
-    Friend WithEvents Label4 As Label
-    Friend WithEvents btn_crearCuenta As Button
+    Friend WithEvents l1 As Guna.UI.WinForms.GunaLabel
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btn_registrar As Guna.UI.WinForms.GunaButton
+    Friend WithEvents txt_saldo As Bunifu.Framework.UI.BunifuMaterialTextbox
+    Friend WithEvents txt_cedula As Bunifu.Framework.UI.BunifuMaterialTextbox
 End Class
